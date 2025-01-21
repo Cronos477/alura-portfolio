438e6f09a97e8d15491896fa1e0c603b0ff82e90
Cronos744 <felipe.chess47@gmail.com>
2025-01-21 16:51:31 -0300

    initialize React app with routing and basic pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/sobremim";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/sobremim" element={<SobreMim/>} />
+        <Route path="*" element={<h1>Página não encontrada</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
